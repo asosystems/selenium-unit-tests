8dddc802d370ee75198ffd39d70a9ba2638fe95a
asotoolsny <asotoolsny@gmail.com>
2019-04-01 00:49:20 -0400

    listing best sellers

--- amazon_helper.py
+++ amazon_helper.py
@@ -1,3 +1,4 @@
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
@@ -6,7 +7,12 @@ from utils import set_text_and_submit
 
 class AmazonHelper():
     search_box_selector = (By.NAME, "field-keywords")
+    search_results_selector = (
+        By.CSS_SELECTOR, ".s-result-list .s-result-item")
+
     links_selector = (By.CSS_SELECTOR, "h5 a.a-link-normal.a-text-normal")
+    badges_selector = (
+        By.CSS_SELECTOR, ".a-badge-text")
 
     def __init__(self, driver):
         self.driver = driver
@@ -20,3 +26,25 @@ class AmazonHelper():
         links = self.driver.find_elements(*self.links_selector)
 
         return links
+
+    def search_and_list_bestsellers(self, query):
+        self.driver.get("https://amazon.com")
+
+        search_text_box = self.driver.find_element(*self.search_box_selector)
+        set_text_and_submit(search_text_box, query)
+
+        # get search results
+        results = self.driver.find_elements(*self.search_results_selector)
+
+        # now filter search results by their badges ("Best Seller")
+        best_sellers = []
+
+        for result in results:
+            try:
+                # if the search result container has a badge, then we add it to the list
+                result.find_element(*self.badges_selector)
+                best_sellers.append(result)
+            except NoSuchElementException:
+                pass
+
+        return best_sellers
--- test_amazon_search.py
+++ test_amazon_search.py
@@ -14,6 +14,16 @@ class Test_AmazonSearch(unittest.TestCase):
 
         self.assertIsNotNone(links)
 
+    def test_can_search_amazon_and_get_bestsellers(self):
+        driver = webdriver.Chrome()
+
+        amazon_searcher = AmazonHelper(driver)
+        items = amazon_searcher.search_and_list_bestsellers("python book")
+
+        print([item.text for item in items])
+
+        self.assertIsNotNone(items)
+
 
 if __name__ == "__main__":
     unittest.main()
